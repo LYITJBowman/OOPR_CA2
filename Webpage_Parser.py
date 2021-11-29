67f9ebe97d0fae1258315047eb6ca90e4c0f56d5
JBowman <L00170244@student.lyit.ie>
2021-11-29 22:12:35 +0000

    Initial web parsing steps

--- Webpage_Parser.py
+++ Webpage_Parser.py
@@ -7,6 +7,8 @@
 #               Available under GNU Public License (GPL)
 # Description : Python script to connect to a remote webpage and retrieve useful information from it
 #
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -22,4 +24,21 @@ if __name__ == '__main__':
     soup = BeautifulSoup(webpage.content, 'html.parser')
 
     # Test print of the results
-    print(soup.prettify())
+    # print(soup.prettify())
+
+    print("The Web Page you have just parsed contains the following sections:")
+    # Retrieve page headings, print just the heading text, stripping whitespace
+    for header in soup.find_all(class_='section_header'):
+        print(header.text.strip())
+
+    # Count the number of times the string Apache2 appears on the page
+    # Note - It doesn't search for a string in the content by default, it needs to be passed as a regular expression
+    apache2_count = soup.find_all(string=re.compile("Apache2"))
+    print("\nThe page contains the string Apache 2 {} times.".format(len(apache2_count)))
+
+    print("\nThe Web Page you have just parsed links to the following addresses:")
+    # Retrieve the target URLs from the webpage
+    # For each URL found, just print the href attribute rather than the full detail
+    for link in soup.find_all("a"):
+        print(link['href'])
+
