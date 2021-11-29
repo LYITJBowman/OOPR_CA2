bb510851930aaeb95f127141f70ce49b39c6ba41
JBowman <L00170244@student.lyit.ie>
2021-11-29 20:26:53 +0000

    First push after initialising

---
+++ Webpage_Parser.py
@@ -0,0 +1,12 @@
+#
+# File        : Webpage_Parser.py  
+# Created     : 29/11/2021
+# Author      : James Bowman
+# Version     : v1.0.0
+# Licensing   : (C) James Bowman
+#               Available under GNU Public License (GPL)
+# Description : 
+#
+
+if __name__ == '__main__':
+    pass
