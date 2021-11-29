ff40cc1e6797e640af5adefac8b392e464b18db5
JBowman <L00170244@student.lyit.ie>
2021-11-29 20:56:14 +0000

    Initial web scrape step added

--- Webpage_Parser.py
+++ Webpage_Parser.py
@@ -5,8 +5,21 @@
 # Version     : v1.0.0
 # Licensing   : (C) James Bowman
 #               Available under GNU Public License (GPL)
-# Description : 
+# Description : Python script to connect to a remote webpage and retrieve useful information from it
 #
+import requests
+from bs4 import BeautifulSoup
+
 
 if __name__ == '__main__':
-    pass
+    #url = input("Please enter the web address you wish to connect to: ")
+    url = 'http://192.168.0.207'
+
+    # Retrieve the Webpage
+    webpage = requests.get(url)
+
+    # Pass the retrieved webpage content through to be parsed by Beautiful Soup
+    soup = BeautifulSoup(webpage.content, 'html.parser')
+
+    # Test print of the results
+    print(soup.prettify())
