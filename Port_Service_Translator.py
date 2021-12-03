3392fbc56ffa203a9d82fd443b2b14f1baad7f8e
JBowman <L00170244@student.lyit.ie>
2021-12-03 22:31:46 +0000

    Scripts tidied and commenting improved. One file removed.

--- Port_Service_Translator.py
+++ Port_Service_Translator.py
@@ -10,22 +10,35 @@ import csv
 
 
 def port_service_translator(port):
+    """
+    Sample function for testing, using local list rather than external file
+    :param port: The port to search for
+    :return: The service name and description for that port
+    """
     port_services = [[22, "ssh", "The Secure Shell (SSH) Protocol"],
                      [80, "http", "World Wide Web HTTP"]]
 
+    # Loop through the list and return details if the port number matches
     for entry in port_services:
         if entry[0] == port:
             return entry[1], entry[2]
 
 
 def port_service_translator_csv(port):
+    """
+    Function that searches an external csv listing from iana.org of ports and their underlying services
+    :param port: The port to search for
+    :return: The service name and description for that port
+    """
+    # Open the CSV File
     fh = open("service-names-port-numbers.csv")
     fr = csv.reader(fh)
+    # Loop through the rows until a port match is found
     for row in fr:
-
         if row[0] == str(port):
             return row[1], row[2]
 
 
 if __name__ == '__main__':
+    # No main as not intended to be run directly
     pass
